7ae02611b6acc99d7253a28a5fb9423b4b08fad6
ricardo <nishimu75618094@gmail.com>
2025-08-11 05:12:53 +0900

    dotenvの修正

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,10 @@ module BlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
-    Bundler.require(*Rails.groups)
-    Dotenv::Rails.load
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Rails.load
+    end
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
